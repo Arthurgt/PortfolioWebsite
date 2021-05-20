ff45b99ec2fc87567b99af405291b14371ec5725
Arthurgt <arturbabkiewicz@gmail.com>
2021-05-20 13:56:21 +0200

    Completed "Experience", "Skills", "Projects" Sections.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,30 @@ let typed = new Typed(".typing", {
     backSpeed: 30,
     loop: true
 });
+
+$(document).ready(function () {
+
+
+    let $btns = $('.projects .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.projects .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.projects .row').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+    $('.projects .button-group #btn1').trigger('click');
+
+    $('.projects .row .test-popup-link').magnificPopup({
+        type: 'image',
+        gallery: { enabled: true }
+    });
+});
