cbc1b5942218bca2a169a4ae66eec23f2bbeff17
Arthurgt <arturbabkiewicz@gmail.com>
2021-05-20 17:15:18 +0200

    Completed "Contact" section and "Footer"

--- js/main.js
+++ js/main.js
@@ -30,4 +30,21 @@ $(document).ready(function () {
         type: 'image',
         gallery: { enabled: true }
     });
+
+    let nav_offset_top = $('.navbar').height() + 50;
+
+    function navbarFixed() {
+        if ($('.navbar').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.navbar').addClass('navbar_fixed');
+                } else {
+                    $('.navbar').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
 });
